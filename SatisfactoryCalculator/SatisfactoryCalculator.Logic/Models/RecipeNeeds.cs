7e62cd420869811981082d7fe21f0e1dc0bbf540
Cody Swendrowski <cody@swendrowski.us>
2019-03-16 21:54:56 -0500

    Adds basic calculator shim

---
+++ SatisfactoryCalculator/SatisfactoryCalculator.Logic/Models/RecipeNeeds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SatisfactoryCalculator.Logic.Models
+{
+    public class RecipeNeeds
+    {
+        public List<Input> Inputs { get; set; } = new List<Input>();
+
+        public Dictionary<string, int> TotalResourceNeeds { get; set; } = new Dictionary<string, int>();
+    }
+}
---
+++ SatisfactoryCalculator/SatisfactoryCalculator.Logic/RecipeCalculator.cs
@@ -0,0 +1,13 @@
+﻿using SatisfactoryCalculator.Logic.Models;
+
+namespace SatisfactoryCalculator.Logic
+{
+    public class RecipeCalculator
+    {
+        public RecipeNeeds CalculateRecipeNeeds(Recipe recipe)
+        {
+            // TODO: recursively get all Inputs and calculate summed total of all resources
+            return new RecipeNeeds();
+        }
+    }
+}
