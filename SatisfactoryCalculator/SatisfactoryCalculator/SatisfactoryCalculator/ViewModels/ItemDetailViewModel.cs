01b3b79b9df4b9e447a2d2179409456d4322f39d
Cody Swendrowski <cody@swendrowski.us>
2019-03-16 20:18:10 -0500

    Initial Xamarin app

---
+++ SatisfactoryCalculator/SatisfactoryCalculator/SatisfactoryCalculator/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SatisfactoryCalculator.Models;
+
+namespace SatisfactoryCalculator.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
